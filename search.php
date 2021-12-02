080bdfbb549a8eeaa8efa1cafb4be6489b922049
Gwang Min <ckm7907@naver.com>
2021-12-02 22:48:08 +0900

    1202

---
+++ search.php
@@ -0,0 +1,53 @@
+<?php
+$result = "";
+$query = "select * from word";
+if (isset($_POST['name'])){
+    $fstr = $_POST['name'];
+    if ($fstr != ''){
+        $query .= " where wordname_kor like '%$fstr%'";
+    }
+}
+try {
+    include_once './db/db.php';
+    $statement = $db->query($query);
+    while($record = $statement->fetch(PDO::FETCH_ASSOC)){
+        $result .= "<tr>";
+        foreach($record as $column){
+            $result .= "<td>" . $column . "</td>";
+        }
+        $result .= "</tr>";
+    }
+} catch(PDOException $e){
+    $result = "#ERR:" . $e->getMessage();
+    echo 'pong';
+}
+$db = null;
+?>
+<!DOCTYPE html>
+<html lang="ko">
+    <head> 
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
+        <title>sample page</title>
+        <style>
+        h1 { font-size:14pt;
+            padding:5px;
+            background-color:#AAFFFF; }
+        table tr td {
+            padding:5px;
+            background-color:#DDFFCC; }
+        </style>
+    </head>
+    <body>
+        <h1>Hello PHP!</h1>
+        <table>
+        <form method="post" action="./search.php">
+            <tr><td>검색 문장:</td><td><input type="text" name="name"></td></tr>
+            <tr><td></td><td><input type="submit" value="송신"></td></tr>
+        </form>
+        </table>
+        <hr>
+        <table>
+        <?php echo $result; ?>
+        </table>
+    </body>
+</html>
---
+++ test_s.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="kr" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css">
+  <link rel="stylesheet" href="test_s.css">
+  <title></title>
+</head>
+
+<body>
+  <form action='search.php' method='POST'>
+    <div class="search-box">
+      <input type="text" class="search-txt" name="" placeholder="Type to search">
+      <a class="search-btn" href="#">
+        <i class="fas fa-search"></i>
+      </a>
+    </div>
+  </form>
+</body>
+
+</html>
